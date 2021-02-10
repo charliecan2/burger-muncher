fd132e8988a244189d4f6fb6c96a055ce2e27576
ccantu20 <carloscq920@gmail.com>
2021-02-09 22:10:58 -0600

    Created a routers for the application in the new file burgers_controller.js

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const burger = require('../models/burger');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    burger.selectAll((data) => {
+        res.render('index', data)
+    });
+});
+
+router.post('/api/burgers', (req, res) => {
+    burger.insertOne([req.body.name, req.body.devoured], (result) => {
+        res.json({id: result.insertId})
+    });
+});
+
+router.put('/api/burgers/:id', (req, res) => {
+    burger.updateOne({
+        devoured: req.body.devoured
+    }, req.params.id,
+    (result) => {
+        if(result.changedRows === 0){
+            return res.status(404).end();
+        }
+        res.status(200).end()
+    });
+});
+
+module.exports = router;
